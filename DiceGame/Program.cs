03e926b4cce2cf8ea46ce058279049838faaf925
Simon S <ssuvemaa@tktk.ee>
2022-03-05 17:54:35 +0200

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem;
+            //mängija kes viskab rohkem, ongi mängu võitja;
+            //täringuid visatakse kokku kolm korda;
+            //programm kuulutab välja võitja;
+            //lõpptulemus ei saa viiki jääda kuna on 3 mängu; 
+
+            Random rnd = new Random();
+
+
+
+
+            //arvuti vise
+            int cpuRandom = rnd.Next(1, 7);
+
+
+
+            //kasutaja vise
+            int userRandom = rnd.Next(1, 7);
+
+            int cpuScore = 0;
+            int userScore = 0;
+            int i = 0;
+
+            while (i < 3)
+            {
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    userScore++;
+                }
+                else
+                {
+                    cpuScore++;
+                }
+
+
+                //arvuti vise
+                cpuRandom = rnd.Next(1, 7);
+
+                //kasutaja vise
+                userRandom = rnd.Next(1, 7);
+                i++;
+
+
+
+            }
+            if (userScore > cpuScore)
+            {
+                Console.WriteLine($"Kasutaja võitis. Võitude arv oli {userScore}.");
+            }
+            else
+            {
+                Console.WriteLine($"Arvuti võitis. Võitude arv oli {cpuScore}.");
+            }
+
+            if (userScore == cpuScore)
+            {
+                Console.WriteLine("Viik!");
+            }
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus ja salasõna;
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admin1234";
+            //siis konsoolis kuvatakse "Tere tulemast!";
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti.";
+            //kasutal on kokku kolm katset;
+            //esimesel, teisel ja kolmandal katsel "Tere tulemast!";
+            //kui esimene, teine ja kolmas katse läks valesti "Vale kasutajatunnus või salasõna. Proovi uuesti.! ;
+
+            int i = 0;
+
+
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+
+                if (userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+
+                    i++;
+                    Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti.");
+                    Console.WriteLine($"Sul on jaanud {3 - i} katset.");
+                }
+            }
+        }
+    }
+}
